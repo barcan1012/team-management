6ae0356efebb9afbd7968c482a6f9b8349ba1f2b
Adrian Barcan <adrian.barcan1012@gmail.com>
2019-12-16 07:09:56 +0200

    Update PersonDTO.java

--- src/main/java/com/team/model/dto/PersonDTO.java
+++ src/main/java/com/team/model/dto/PersonDTO.java
@@ -11,9 +11,4 @@ public class PersonDTO {
     private String firstName;
     private String lastName;
 //    private String team;
-
-//    public PersonDTO(String firstName, String lastName) {
-//        this.firstName = firstName;
-//        this.lastName = lastName;
-//    }
 }
